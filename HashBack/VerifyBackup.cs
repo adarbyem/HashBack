9bf0b7f9e1089d0a1b2cb32fd71fcc9d900163d9
adarbyem <aaron.d.darby@gmail.com>
2017-03-31 15:24:48 -0700

    Update 0.1.1
    - Added some UI functionality.
    - Modified the Crypto File to prevent excess memory from being used when hashing large files.
    - Cleaned up some code.

--- HashBack/VerifyBackup.cs
+++ HashBack/VerifyBackup.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Windows.Forms;
-using System.Text;
-using System.Threading.Tasks;
 using System.Data.SqlServerCe;
 
 namespace HashBack
